47b4b9704e50ea49963d904bcc4f43e48c7b7a10
Sam Wang <samwang@Sams-MBP.lan>
2022-05-19 14:45:39 -0500

    heroku stuff

--- index.js
+++ index.js
@@ -158,6 +158,6 @@ app.post("/failure", function(req,res){
   res.redirect("/");
 })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT ||  3000, function() {
   console.log("running on 3000");
 })
