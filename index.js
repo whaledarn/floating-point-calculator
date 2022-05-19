9b9542545979fd93c94f9a2fd307f2f1cf32656d
Sam Wang <samwang@Sams-MBP.lan>
2022-05-19 13:18:25 -0500

    added functions for floats

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 const app = express();
 
@@ -13,6 +15,8 @@ app.get("/", function(req, res) { // Immediately loads html page
 
   res.sendFile(__dirname + "/index.html");
 })
+global.document = new JSDOM("/index.html").window.document;
+
 
 function isValid(p, q, sig, pbits, qbits){
   if(sig != 0 || sig != 1)
@@ -22,16 +26,68 @@ function isValid(p, q, sig, pbits, qbits){
   return true;
 }
 
+
+
+function isDenormalized(pbits,qbits){
+  for(var i = 0; i<pbits.length; i++){
+    if(pbits[i]=='1')
+      return false;
+  }
+}
+
+function isInfinity(pbits, qbits){
+  for(var i = 0; i<pbits.length; i++){
+    if(pbits[i]=='0')
+      return false;
+  }
+  for(var i = 0; i<qbits.length; i++){
+    if(qbits[i]=='1')
+      return false;
+  }
+  return true;
+}
+
+function isNaN(pbits, qbits){
+  for(var i = 0; i<pbits.length; i++){
+    if(pbits[i]=='0')
+      return false;
+  }
+  for(var i = 0; i<qbits.length; i++){
+    if(qbits[i]=='1')
+      return true;
+  }
+  return false;
+}
+
+function calculateNormalized(s,p,b){
+
+}
+
+function calculateDenormalized(s,p,b){
+
+}
+
 app.post("/", function(req, res) { // Called when button is pressed
-  var p = req.body.p;
-  var q = req.body.q;
-  var sig = req.body.sig;
-  var pbits = req.body.pbits;
-  var qbits = req.body.qbits;
+  var sig = req.body.s;
+  var pbits = req.body.p;
+  var qbits = req.body.q;
 
-  res.write("<div class='row g-4 py-5 row-cols-1 row-cols-lg-3'>" + "penis" +"</div>")
+  console.log(typeof(pbits));
 
-  res.send();
+  if(isInfinity(pbits,qbits))
+    console.log("Is infinity");
+  else if(isNaN(pbits,qbits))
+    console.log("Is NaN");
+  else if(isDenormalized(pbits,qbits))
+    console.log("Is denormalized");
+    else
+      console.log("Is normalized");
+
+
+  document.getElementById("answer").style.display = "inline";
+  // res.write("<div class='row g-4 py-5 row-cols-1 row-cols-lg-3'>" + "penis" +"</div>")
+
+  res.sendFile(__dirname + "/index.html");
 })
 
 app.listen(3000, function() {
